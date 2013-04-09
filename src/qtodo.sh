bd06e38bcb59fc719aca5bf2b4662af6b492a416
Ruediger Gad <r.c.g@gmx.de>
2013-04-09 08:08:19 +0200

    Kill pid of messageserver process instead of using killall.

--- src/qtodo.sh
+++ src/qtodo.sh
@@ -5,7 +5,7 @@ export QML_IMPORT_PATH="${QTODO_DIR}/lib/imports"
 export QMF_PLUGINS="${QTODO_DIR}/lib/qmf/plugins"
 export LD_LIBRARY_PATH="${QTODO_DIR}/lib/qmf/lib"
 
-lib/qmf/bin/messageserver &
+eval "${QTODO_DIR}/lib/qmf/bin/messageserver &"
+MESSAGESERVER_PID=$!
 bin/qtodo
-# TODO: Should only kill pid of above messageserver process here.
-killall messageserver
+kill $MESSAGESERVER_PID
