8ab9cddcbbe7cc7ac33bc653cb9ec76aaa469e1c
Ruediger Gad <r.c.g@gmx.de>
2013-04-30 14:12:34 +0200

    Update build script for MeeGo version.

--- src/build.sh
+++ src/build.sh
@@ -4,4 +4,10 @@ find . -name .svn -exec rm -rf {} \;
 mv qtc_packaging/debian_harmattan debian
 mv debian/sb_rules debian/rules
 rm -rf qtc_packaging/
+
+mv lib/build .
+rm -rf lib
+mkdir lib
+mv build lib
+
 fakeroot dpkg-buildpackage -sa
