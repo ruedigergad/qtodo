70af8e657d57bb6079fa139df44085d86e00903a
Ruediger Gad <r.c.g@gmx.de>
2013-05-01 11:32:47 +0200

    Fix qml paths for Windows version.

--- src/prepare_windows_build.sh
+++ src/prepare_windows_build.sh
@@ -1,6 +1,7 @@
 #/bin/sh
 
-find qml/ -exec sed -i 's/\.\.\/icons/qrc:/g' {} \;
+find qml/ -exec sed -i 's/\.\.\/icons/qrc:\/icons/g' {} \;
+find qml/ -exec sed -i '/^import.*common.*/d' {} \;
 tar czf ../windows_source.tar.gz ../src
 git reset --hard
 
