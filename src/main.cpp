86b83e0e6d7e0af7870547b0a20bcfe10ed0c8bb
Ruediger Gad <r.c.g@gmx.de>
2013-05-03 00:12:41 +0200

    Set text codec also for locale and tr.

--- src/main.cpp
+++ src/main.cpp
@@ -74,6 +74,8 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 #endif
 
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("UTF-8"));
 
     /*
      * Splash screen for desktop versions.
